b65d0e112c3b617c4af305217ab30d8a75d0fa38
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-06 10:27:05 +0100

    about page+

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,13 +3,17 @@ import { Avatar, Container, Typography } from '@mui/material'
 import React from 'react'
 
 function About() {
+  const pStyles =  {opacity: '.8', fontSize: '1.1em'}
   return (
     <Container align='center' sx={{marginTop: '95px'}}>
-        <Avatar sx={{ bgcolor: "lightblue" }}><Info/></Avatar>
+        <Avatar sx={{ bgcolor: "#42a5f5" }}><Info/></Avatar>
 
         <Typography variant='h1' className='about-title'  sx={{marginTop: '5px', fontWeight: '500'}} gutterBottom>About</Typography>
 
-        <Typography variant='p' sx={{opacity: '.8', fontSize: '1.1em'}} paragraph>After using CRA [create react app] for a while, I ran into a problem with pollyfilling in webpack and it made me realise I knew little to nothing about configuration files, after promptly fixing the bug I decided to look into alternative react enviroment creaters and ran across vite, the rest is self explainatory </Typography>
+        <Typography variant='p' sx={pStyles} gutterBottom paragraph>After using CRA [create react app] for a while, I ran into a problem with pollyfilling in webpack and it made me realise how little I knew about building react applications, in order to build my comprenesion I thought it'd be a good idea to mess around with Vite.</Typography>
+
+        <Typography variant='h3' sx={{marginTop: '1em', fontWeight: '500'}} gutterBottom>What is Vite?</Typography>
+        <Typography variant='p' sx={pStyles}>Vite, which was created by Evan You, the creator of the Vue.js framework, is next-generation frontend tooling that provides a blazing-fast dev server, bundling and a great developer experience. When I say fast, I mean it—as start-up times can decrease tenfold in comparison to other tools, such as webpack, Rollup or Parcel.</Typography>
 
 
     </Container>
